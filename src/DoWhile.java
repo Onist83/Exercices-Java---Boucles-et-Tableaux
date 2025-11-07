7070898f40bd7b46f22e3d80f8322bb29735ff9a
Stineau <t.stineau@outlook.fr>
2025-11-07 16:43:26 +0100

    Correction while + exo do while

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,2 +1,109 @@
+import java.util.Scanner;
+
 public class DoWhile {
+
+    // Exercice 10
+ /*   public static void menu () {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+         do {
+            affichageMenu();
+            choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Bonjour");
+             } else if (choice == 2) {
+                 System.out.println("Au revoir");
+             }
+
+         } while (choice != 3);
+        System.out.println("Exit");
+    }
+
+    public static void affichageMenu() {
+        System.out.println("What choice do you want to do?");
+        System.out.println("Choice 1: Bonjour");
+        System.out.println("Choice 2: Au revoir");
+        System.out.println("Choice 3: Exit");
+    }
+
+
+    public static void main (String[]args){
+        menu ();
+    } */
+
+    // Exercice 11 -
+
+ /*   public static void converter() {
+
+        Scanner scanner = new Scanner(System.in);
+        affichageMenu();
+
+        int choice = scanner.nextInt();
+
+
+        do {
+            if (choice == 1) {
+                System.out.println("Celsius-->Fahrenheit");
+                convertir(1);
+                affichageMenu();
+                choice = scanner.nextInt();
+
+
+            } else if (choice == 2) {
+                System.out.println("Fahrenheit-->Celsius");
+                convertir(2);
+                affichageMenu();
+                choice = scanner.nextInt();
+
+            } else if (choice > 3 || choice == 0) {
+                System.out.println("Choix non valide");
+                affichageMenu();
+                choice = scanner.nextInt();
+            }
+        }
+            while (choice != 3) ;
+            System.out.println("Exit");
+
+
+    }        public static void affichageMenu() {
+        System.out.println("What choice do you want to do?");
+        System.out.println("Choice 1: Celsius-->Fahrenheit");
+        System.out.println("Choice 2: Fahrenheit-->Celsius");
+        System.out.println("Choice 3: Exit");
+    }
+
+        public static void convertir(int choice) {
+            Scanner scanner = new Scanner(System.in);
+            int numb = scanner.nextInt();
+            int conversion1 = (numb*9/5)+32 ;
+            int conversion2 =(numb-32)*5/9 ;
+
+
+
+                if (choice == 1) {
+                    System.out.println("The temperature is " + conversion1  + " Fahrenheit");
+                } else if (choice == 2) {
+                    System.out.println("The temperature is " + conversion2  + " Celsius");
+                }
+
+        }
+
+
+
+    public static void main(String[] args) {
+        converter();
+    } */
+
+
+    // Exercice 12 -
+
+
+    public static void menu () {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+
+
+
+
 }
--- src/While.java
+++ src/While.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
-public class While {
+/*    public class While {
     // Exercice 6 -
 
-    public static void guess() {
+    /*public static void guess() {
         boolean run = true;
         Scanner scanner = new Scanner(System.in);
         affichageMenu();
 
         int choice = scanner.nextInt();
-        int phrase;
 
 
         while (run) {
@@ -17,7 +16,6 @@ public class While {
                 case 1:
                     System.out.println("Guess my number");
                     System.out.println("Choice 1: Write a number");
-                    phrase = scanner.nextInt();
                     jeux();
                     affichageMenu();
                     choice = scanner.nextInt();
@@ -28,34 +26,292 @@ public class While {
                     affichageMenu();
                     choice = scanner.nextInt();
                     break;
+
+                case 2:
+                    System.out.println("Exit");
+                    run = false;
+                    break;
             }
         }
     }
 
     public static void affichageMenu() {
         System.out.println("What choice do you want to do?");
-        System.out.println("Choice 1: Write a number for a multiplication");
+        System.out.println("Choice 1: Write a number for guess my number");
         System.out.println("Choice 2: Exit");
     }
 
     public static void jeux() {
-        int numb = ;
+        Scanner scanner = new Scanner(System.in);
+        int numb = scanner.nextInt();
         int randomNum = (int) (Math.random() * 101);
-        if (i > randomNum) {
-            System.out.println("My number is smaller");
-            numb = scanner.nextInt();
-        } else if (i < randomNum) {
-            System.out.println("My number is greater");
-            numb = scanner.nextInt();
-        } else {
-            System.out.println("You Win");
-        }
+        boolean run2 = true;
 
+        while (run2) {
+
+            if (numb > randomNum) {
+                    System.out.println("My number is smaller");
+                    numb = scanner.nextInt();
+                } else if (numb < randomNum) {
+                    System.out.println("My number is greater");
+                    numb = scanner.nextInt();
+                } else {
+                    System.out.println("You Win");
+                    run2 = false;
+                }
+
+        }
     }
-}
+
 
 
     public static void main(String[] args) {
         guess();
+        } */
+
+
+    // Exercice 7 -
+
+  /*  public static void addition() {
+
+        boolean run = true;
+        Scanner scanner = new Scanner(System.in);
+        affichageMenu();
+
+        int choice = scanner.nextInt();
+
+        while (run) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Addition");
+                    System.out.println("Choice 1: Write a number");
+                    numbers();
+                    affichageMenu();
+                    choice = scanner.nextInt();
+                    break;
+
+                default:
+                    System.out.println("Choix non valide");
+                    affichageMenu();
+                    choice = scanner.nextInt();
+                    break;
+
+                case 2:
+                    System.out.println("Exit");
+                    run = false;
+                    break;
+            }
+        }
     }
-}
+
+        public static void affichageMenu () {
+            System.out.println("What choice do you want to do?");
+            System.out.println("Choice 1: Write a number");
+            System.out.println("Choice 2: Exit");
+        }
+
+        // il faut un return
+        // addition de chaque nombre
+        // resultat final en appuyant sur 0
+
+        public static int numbers () {
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            boolean run2 = true;
+            int result = 0;
+
+            while (run2) {
+                if (number == 0) {                     // result+=i
+                    System.out.println("You result is : " + result);
+                    run2 = false;
+                } else {
+                    result += number;
+                    System.out.println("You result is : " + result);
+                    System.out.println("Enter a new number to add it");
+                    number = scanner.nextInt();
+                }
+            } return result;
+        }
+
+
+
+
+
+
+
+
+        public static void main (String[]args){
+            addition();
+        } */
+
+
+    // Exercice 8 -
+/*
+    public static void compte() {
+
+        boolean run = true;
+        Scanner scanner = new Scanner(System.in);
+        affichageMenu();
+
+        int choice = scanner.nextInt();
+
+        while (run) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Compte");
+                    System.out.println("Choice 1: Write a number");
+                    numbs();
+                    affichageMenu();
+                    choice = scanner.nextInt();
+                    break;
+
+                default:
+                    System.out.println("Choix non valide");
+                    affichageMenu();
+                    choice = scanner.nextInt();
+                    break;
+
+                case 2:
+                    System.out.println("Exit");
+                    run = false;
+                    break;
+            }
+        }
+    }
+
+    public static void affichageMenu() {
+        System.out.println("What choice do you want to do?");
+        System.out.println("Choice 1: Write a number");
+        System.out.println("Choice 2: Exit");
+    }
+
+    public static int numbs() {
+        boolean run2 = true;
+        Scanner scanner = new Scanner(System.in);
+        int numbs = scanner.nextInt();
+        int count = 0;
+
+// compteur pour comptabiliser le nombre de chiffres "count" et sa valeur "countValue"
+// numbs = count
+
+        while (run2) {
+            if (numbs != 0) {
+                numbs /= 10 ;
+                count++;
+                } else {
+                run2 = false;
+                }
+            }   System.out.println("There are " + count + "  numbers.");
+                return count ;
+        }
+
+
+
+
+    public static void main (String[]args){
+        compte();
+    }
+*/
+
+    // Exercice 9 -
+
+ /*   public static void primeOrNot() {
+
+        boolean run = true;
+        Scanner scanner = new Scanner(System.in);
+        affichageMenu();
+
+        int choice = scanner.nextInt();
+
+        while (run) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Prime numbers or not");
+                    System.out.println("Choice 1: Write a number");
+                    prime();
+                    affichageMenu();
+                    choice = scanner.nextInt();
+                    break;
+
+                default:
+                    System.out.println("Choix non valide");
+                    affichageMenu();
+                    choice = scanner.nextInt();
+                    break;
+
+                case 2:
+                    System.out.println("Exit");
+                    run = false;
+                    break;
+            }
+        }
+    }
+        public static void affichageMenu () {
+            System.out.println("What choice do you want to do?");
+            System.out.println("Choice 1: Write a number");
+            System.out.println("Choice 2: Exit");
+        }
+
+
+        public static void prime () {
+            Scanner scanner = new Scanner(System.in);
+            int numbers = scanner.nextInt();
+            boolean run2 = true;
+
+            while (run2) {
+
+                if (numbers == 0) {
+                    System.out.println("Pas premier");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers == 1) {
+                    System.out.println("Peut etre");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers == 2) {
+                    System.out.println("Pas premier");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers == 3) {
+                    System.out.println("Peut etre");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers == 5) {
+                    System.out.println("Peut etre");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers == 7) {
+                    System.out.println("Peut etre");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers % 2 == 0) {
+                    System.out.println("Pas premier");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers % 3 == 0) {
+                    System.out.println("Pas premier");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers % 5 == 0) {
+                    System.out.println("Pas premier");
+                    numbers = scanner.nextInt();
+
+                } else if (numbers % 7 == 0) {
+                    System.out.println("Pas premier");
+
+                } else {
+                    System.out.println("Peut etre");
+                    run2 = false;
+                }
+            }
+        }
+
+                public static void main (String[]args){
+                    primeOrNot();
+                }
+            } */
+
+
+
+
+
